845250bb92ffcfd362f9b90d2690e46bb89ba19c
tarrasjose <104996954+tarrasjose@users.noreply.github.com>
2022-09-08 18:56:32 -0300

    atualização

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function gerarTabuada() {
     var num = document.getElementById('txtn')
+    var escreva = document.getElementById('res')
+    
+    if(num.value.length == 0 ) {
+        alert('Por favor, digite um número')
+    } else {
+        var n = Number(num.value)
+
+        escreva.innerHTML = ''
+
+        for(var c = 1; c <= 10; c++) {
+            escreva.innerHTML += `${n} x ${c} = ${n*c} <br>`
+        }
+    }
+    
+    
+    /*var num = document.getElementById('txtn')
     var tab = document.getElementById('seltab')
 
     if(num.value.length == 0 ) {
@@ -14,7 +30,7 @@ function gerarTabuada() {
             item.text= `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
         }
-    }
+    }*/
 }
 
 
